cc33ffa5b417bfac0633c5b38a585c8e25f1070f
Illgner José <illgnerjosbatista@gmail.com>
2025-10-26 18:45:31 -0300

    feat: adiciona rota de usuario no backend para alimentar o pinia

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TripController;
 use App\Http\Controllers\LoginController;
@@ -7,10 +8,14 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\StatusController;
 use App\Http\Controllers\DestinationController;
 
-Route::post('register', RegisterController::class);
-Route::post('login', LoginController::class);
+Route::post('register', RegisterController::class)->name('register');
+Route::post('login', LoginController::class)->name('login');
 
 Route::middleware('auth:sanctum')->group( function () {
+    Route::get('user', function (Request $request) {
+            return $request->user();
+        });
+
     Route::resource('trips', TripController::class)
         ->except([
             'destroy', 'create', 'edit'
