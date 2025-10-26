f14ccda868b0b2637118bfcdf897162aee56e3c6
Illgner José <illgnerjosbatista@gmail.com>
2025-10-25 23:11:24 -0300

    feat: adiciona rotas

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,13 +1,30 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TripController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\StatusController;
+use App\Http\Controllers\DestinationController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('register', RegisterController::class);
+Route::post('login', LoginController::class);
 
-Route::resource('trips', TripController::class)->except([
-    'destroy', 'create', 'edit'
-]);
+Route::middleware('auth:sanctum')->group( function () {
+    Route::resource('trips', TripController::class)
+        ->except([
+            'destroy', 'create', 'edit'
+        ]);
+
+    Route::resource('statuses', StatusController::class)
+        ->only([
+            'index',
+        ]);
+    
+    Route::resource('destinations', DestinationController::class)
+        ->only([
+            'index',
+        ]);
+});
+
+
