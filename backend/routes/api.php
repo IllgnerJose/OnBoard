b161f0640a13f1ee7312b96932b5d6670c34d51e
root <root@WKS15002.ias.intranet>
2025-10-27 13:41:55 -0300

    refactor: altera formatacao de texto na rota da api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -19,27 +19,27 @@ Route::middleware('auth:sanctum')->group( function () {
     Route::put('/notifications/{id}/read', [NotificationController::class, 'markAsRead']);
     Route::put('/notifications/mark-all-read', [NotificationController::class, 'markAllAsRead']);
 
-    Route::get('user', function (Request $request) {
-            return new UserResource($request->user());
-        });
+    Route::get('/user', function (Request $request) {
+        return new UserResource($request->user());
+    });
 
-    Route::resource('trips', TripController::class)
+    Route::resource('/trips', TripController::class)
         ->except([
             'destroy', 'create', 'edit', 'update'
         ]);
 
-    Route::put("trips/approve/{trip}", [TripController::class, 'approve'])
+    Route::put("/trips/approve/{trip}", [TripController::class, 'approve'])
         ->name('trip.approve');    
 
-    Route::put("trips/cancel/{trip}", [TripController::class, 'cancel'])
+    Route::put("/trips/cancel/{trip}", [TripController::class, 'cancel'])
         ->name('trip.cancel');   
 
-    Route::resource('statuses', StatusController::class)
+    Route::resource('/statuses', StatusController::class)
         ->only([
             'index',
         ]);
     
-    Route::resource('destinations', DestinationController::class)
+    Route::resource('/destinations', DestinationController::class)
         ->only([
             'index',
         ]);
